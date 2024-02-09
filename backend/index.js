e79705264a7abb05dac5cd5a6aa958b7885946f9
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2024-02-10 03:25:18 +0530

    backend : route events

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,21 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/authRouter.js");
 const productRoutes = require("./routes/productRoutes.js");
+const eventRoutes = require("./routes/eventRoutes.js");
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-	cors({
-		origin: ["http://localhost:5173"],
-		credentials: true,
-	})
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/events")
+app.use("/api/events", eventRoutes);
 connectDB();
 
 app.listen(5000, () => console.log("Server running on port 5000"));
