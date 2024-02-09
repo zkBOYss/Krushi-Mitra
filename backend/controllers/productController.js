4843d83dd17f96d86c47554d2a743a8c2d3d5ce9
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-02-10 03:15:05 +0530

    backend: update product(by id) endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,7 +36,44 @@ const getSingleProduct = async (req, res) => {
 	}
 };
 
+const updateProduct = async (req, res) => {
+	const { productId } = req.params;
+	const { name, description, price, quantity, imageUrl } = req.body;
+
+	try {
+		const product = await Product.findByIdAndUpdate(
+			productId,
+			{
+				name,
+				description,
+				price,
+				quantity,
+				imageUrl,
+				sellerId: req.user.id,
+				buyerId: req.user.id,
+				isSold: true,
+			},
+			{
+				new: true,
+			}
+		);
+
+		if (!product) {
+			return res.status(404).json({
+				message: "Product not found!",
+			});
+		}
+
+		res.json(product);
+	} catch (error) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
 module.exports = {
 	getAllProducts,
 	getSingleProduct,
+	updateProduct,
 };
