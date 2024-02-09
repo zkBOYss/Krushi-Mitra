9895271e56de868caca85c20ba657ada44071900
imsp18 <shivangparmar537@gmail.com>
2024-02-10 02:22:47 +0530

    Frontend: UI changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-
+    
   return (
     <div
-      className="flex py-5 shadow-lg rounded-2xl mx-2 sticky top-2 backdrop-blur-lg z-40
+      className="flex py-5 rounded-2xl mx-2 sticky top-2 backdrop-blur-lg z-40
 	"
     >
       <div className="container mx-auto flex justify-between px-6">
         <Link
           to="/"
-          className="lg:text-3xl md:text-3xl text-lg font-bold text-lightred font-grotesk cursor-pointer"
+          className=" flex lg:text-4xl items-center gap-1  text-lg text-lightred  cursor-pointer" style={{fontFamily : "Vestige Grotesk"}}
         >
-          Logo
+            <img src={logo} alt="logo" className="w-11 h-12 max-sm:w-6 max-sm:h-6" />
+          Krushi Mitra
         </Link>
         <div
           className={`lg:flex md:flex lg:items-center md:items-center lg:space-x-4 md:space-x-4 md:py-1 lg:py-1 lg:text-base md:text-base lg:text-black md:text-black lg:font-medium font-poppins  md:font-medium hidden
