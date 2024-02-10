ffb615967eedd4551e9fbd0136e3c15e9df74393
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-02-11 03:36:42 +0530

    frontend/bug: fixed product id

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -37,7 +37,7 @@ const Marketplace = () => {
 				price: item.price,
 				quantity: item.quantity,
 				sellerId: item.sellerId,
-				_id: item._id,
+				_id: item.id,
 			}));
 
 			console.log("Mapped: ", mappedData);
