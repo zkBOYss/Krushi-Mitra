49b5c03afae2d704b722be8eb58c835db017e06b
imsp18 <shivangparmar537@gmail.com>
2024-02-10 11:31:52 +0530

    Frontend: added login button & fixed issues

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,85 +1,107 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import LogoutButton from "./LogoutButton";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const user = useAuthContext();
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div className="py-5 shadow-lg rounded-2xl mx-2 sticky top-2 backdrop-blur-lg z-40">
-      <div className="container mx-auto flex justify-between items-center px-6">
-        <Link
-          to="/"
-          className="flex items-center text-lg text-lightred font-grotesk cursor-pointer"
-        >
-          <img src={logo} alt="Logo" className="w-12 h-12 max-sm:w-8 max-sm:h-8 mr-2" />
-          <span className="lg:text-3xl md:text-3xl">Krushi Mitra</span>
-        </Link>
-        <div className="md:hidden">
-          <button
-            type="button"
-            className="text-gray-500 hover:text-lightyellow focus:outline-none focus:text-lightyellow"
-            onClick={toggleNavbar}
-          >
-            <svg
-              className="h-8 w-8 fill-current"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {isOpen ? (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M19 14H5v-1h14v1zm0-5H5v1h14V9zm0-5H5v1h14V4z"
-                />
-              ) : (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4 6h16v-1H4v1zm0 5h16V10H4v1zm0 5h16v-1H4v1z"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
+    return (
         <div
-          className={`md:flex ${isOpen ? 'block' : 'hidden'} md:block lg:items-center md:items-center lg:space-x-4 md:space-x-4 md:py-1 lg:py-1 lg:text-base md:text-base lg:text-black md:text-black lg:font-medium font-poppins  md:font-medium`}
+            className="flex py-5 shadow-lg rounded-2xl sticky w-full top-2 backdrop-blur-lg z-40
+	"
         >
-          <Link to="/" className="py-1 px-2 hover:text-lightyellow ease-in transition-all">
-            Home
-          </Link>
-          <Link
-            to="/dashboard"
-            className="py-1 px-2 hover:text-lightyellow ease-in transition-all"
-          >
-            Dashboard
-          </Link>
-          <Link
-            to="/marketplace"
-            className="py-1 px-2 hover:text-lightyellow ease-in transition-all"
-          >
-            Marketplace
-          </Link>
-          <Link
-            to="/events"
-            className="py-1 px-2 hover:text-lightyellow ease-in transition-all"
-          >
-            Events
-          </Link>
-          <Link
-            to="/add-products"
-            className="py-1 px-2 hover:text-lightyellow ease-in transition-all"
-          >
-            Add Product
-          </Link>
+            <div className="container mx-auto flex justify-between px-6">
+                <Link
+                    to="/"
+                    className="flex items-center lg:text-3xl md:text-3xl text-lg font-bold text-lightred font-grotesk cursor-pointer" style={{fontFamily: "Vestige Grotesk"}}
+                >
+                    <img src={logo} alt="Logo" className="w-12 h-12 max-sm:w-8 max-sm:h-8 mr-2" />
+                    <span className="lg:text-3xl md:text-3xl">Krushi Mitra</span>
+
+                </Link>
+                <div
+                    className={`lg:flex md:flex lg:items-center md:items-center lg:space-x-4 md:space-x-4 md:py-1 lg:py-1 lg:text-base md:text-base lg:text-black md:text-black lg:font-medium font-poppins  md:font-medium hidden
+		  `}
+                >
+                    <Link to="/" className="py-1 px-2  hover:text-lightyellow ease-in transition-all">
+                        Home
+                    </Link>
+                    <Link to="/dashboard" className="py-1 px-2  hover:text-lightyellow ease-in transition-all ">
+                        Dashboard
+                    </Link>
+                    <Link to="/marketplace" className="py-1 px-2  hover:text-lightyellow ease-in transition-all ">
+                        Marketplace
+                    </Link>
+                    <Link to="/events" className="py-1 px-2  hover:text-lightyellow ease-in transition-all ">
+                        Events
+                    </Link>
+                    <Link to="/add-products" className="py-1 px-2  hover:text-lightyellow ease-in transition-all">
+                        Add Product
+                    </Link>
+                </div>
+                {toggle && toggle ? (
+                    <div
+                        className={`${toggle
+                                ? " flex w-full flex-col bg-lightgreen rounded-b-2xl items-center font-poppins  absolute right-0 top-16 md:hidden lg:hidden"
+                                : "hidden"
+                            }`}
+                    >
+                        <Link to="/" className="py-1 px-2 hover:text-white">
+                            Home
+                        </Link>
+                        <Link to="/dashboard" className="py-1 px-2 hover:text-white">
+                            Dashboard
+                        </Link>
+                        <Link to="/marketplace" className="py-1 px-2 hover:text-white ">
+                            Marketplace
+                        </Link>
+                        <Link to="/events" className="py-1 px-2 hover:text-white ">
+                            Events
+                        </Link>
+                        <Link to="/add-products" className="py-1 px-2 hover:text-white">
+                            Add Product
+                        </Link>
+                        {user ? (
+                            <Link to="/sign-in" className="py-1 px-2 hover:text-white">
+                                Logout
+                            </Link>
+                        ) : (
+                            <Link to="/sign-in" className="py-1 px-2 hover:text-white">
+                                Sign In
+                            </Link>
+                        )}
+                    </div>
+                ) : null}
+
+                <div
+                    onClick={() => setToggle(!toggle)}
+                    className="flex items-center gap-1 flex-col lg:hidden md:hidden cursor-pointer"
+                >
+                    <span className="flex w-7 h-1 bg-lightgreen"></span>
+                    <span className="flex w-7 h-1 bg-lightgreen"></span>
+                    <span className="flex w-7 h-1 bg-lightgreen"></span>
+                </div>
+                <div className="hidden space-x-4 lg:flex md:flex items-center">
+                    {user && user.userId ? (
+                        <Link to="/">
+                            <LogoutButton />
+                        </Link>
+                    ) : (
+                        <Link
+                            to="/sign-in"
+                            className="text-lg bg-lightgreen py-1 px-6 text-black font-semibold font-grotesk
+              rounded-lg  hover:bg-lightred  hover:text-white transition-all"
+                        >
+                            Sign In
+                        </Link>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
---
+++ frontend/src/components/LogoutButton.jsx
@@ -0,0 +1,28 @@
+const LogoutButton = () => {
+	const logout = async () => {
+		const response = await fetch("http://localhost:5000/api/auth/logout", {
+			credentials: "include",
+			method: "POST",
+		});
+		if (!response.ok) {
+			throw new Error("Error during logging out!");
+		} else {
+			alert("Logged out successfully!");
+			window.location.reload();
+		}
+	};
+	const handleClick = () => {
+		logout();
+	};
+	return (
+		<button
+			onClick={handleClick}
+			className="text-lg bg-lightgreen py-1 px-6 text-black font-semibold font-grotesk
+			rounded-lg  hover:bg-lightred  hover:text-white transition-all"
+		>
+			Logout
+		</button>
+	);
+};
+
+export default LogoutButton;    
