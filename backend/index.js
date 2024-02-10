afec2e2e4c718b6a4613f513c9fdf389f75f59a2
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2024-02-10 10:50:05 +0530

    backend: minor bug fixes

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
 const cookieParser = require("cookie-parser");
+const cloudinary = require("cloudinary");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const authRoutes = require("./routes/authRouter.js");
 const productRoutes = require("./routes/productRoutes.js");
-const eventRoutes = require("./routes/eventRoutes.js");
+const eventRoutes = require("./routes/eventRouter.js");
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-	cors({
-		origin: ["http://localhost:5173"],
-		credentials: true,
-	})
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
 );
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 app.use("/api/auth", authRoutes);
