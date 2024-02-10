ac7ca324987703cc17e5c8fb0d339f8d39217550
imsp18 <shivangparmar537@gmail.com>
2024-02-10 21:01:59 +0530

    Frontend: UI fixes & changes to Krushi Mitra

--- frontend/src/forms/ManageEventForm/EventDetails.jsx
+++ frontend/src/forms/ManageEventForm/EventDetails.jsx
@@ -2,6 +2,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { useAuthContext } from "../../context/AuthContext";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+import addEvent from "../../assets/add_task.svg"
 
 const EventDetails = () => {
 	const user = useAuthContext();
@@ -67,146 +68,171 @@ const EventDetails = () => {
 				onSubmit={onSubmit}
 				encType="multipart/form-data"
 			>
-				<h1 className="text-3xl font-bold mb-3">Add Event</h1>
-				<label className="text-gray-700 text-md font-bold flex-1">
-					Name
-					<input
-						type="text"
-						className="border rounded w-full py-1 px-2 font-normal"
-						{...register("name", {
-							required: "Field is required",
-						})}
-					/>
-					{errors.name && (
-						<span className="text-red-500 text-sm">
-							{errors.name.message}
-						</span>
-					)}
-				</label>
-
-				<div className="flex gap-4">
-					<label className="text-gray-700 text-md font-bold flex-1">
-						Organizer Name
-						<input
-							type="text"
-							className="border rounded w-full py-1 px-2 font-normal"
-							{...register("organizer", {
-								required: "Field is required",
-							})}
-						/>
-						{errors.organizer && (
-							<span className="text-red-500 text-sm">
-								{errors.organizer.message}
-							</span>
-						)}
-					</label>
-					<label className="text-gray-700 text-md font-bold flex-1">
-						Venue
-						<input
-							type="text"
-							className="border rounded w-full py-1 px-2 font-normal"
-							{...register("venue", {
-								required: "Field is required",
-							})}
-						/>
-						{errors.venue && (
-							<span className="text-red-500 text-sm">
-								{errors.venue.message}
-							</span>
-						)}
-					</label>
-				</div>
-
-				<label className="text-gray-700 text-md font-bold flex-1">
-					Description
-					<textarea
-						rows={10}
-						className="border rounded w-full py-1 px-2 font-normal"
-						{...register("description", {
-							required: "Field is required",
-						})}
-					/>
-					{errors.description && (
-						<span className="text-red-500 text-sm">
-							{errors.description.message}
-						</span>
-					)}
-				</label>
-				<div className="flex gap-4">
-					<label className="text-gray-700 text-md font-bold flex-1">
-						Select Time
-						<input
-							type="text"
-							className="border rounded w-full py-1 px-2 font-normal"
-							{...register("time", {
-								required: "Field is required",
-							})}
-						/>
-						{errors.time && (
-							<span className="text-red-500 text-sm">
-								{errors.time.message}
-							</span>
-						)}
-					</label>
-					<label className="text-gray-700 text-md font-bold flex-1">
-						Event Date
-						<DatePicker
-							required
-							selected={eventDate}
-							onChange={(date) => setValue("eventDate", date)}
-							selectsStart
-							startDate={eventDate}
-							minDate={minDate}
-							maxDate={maxDate}
-							placeholderText="Enter Date"
-							className="bg-white p-2 focus:outline:none border rounded-md border-gray-700"
-							wrapperClassName="datePicker"
-						/>
-					</label>
-				</div>
-				{/* Images logic */}
-				<label className="text-gray-700 text-md font-bold ">
-					<div>
-						<h2 className="text-2xl font-bold mb-3">
-							Upload Image for reference
-						</h2>
-						<div className="border rounded p-4 flex flex-col gap-4">
-							<input
-								type="file"
-								multiple
-								accept="image/*"
-								className="w-full text-gray-700 font-normal"
-								{...register("imageFile", {
-									validate: (imageFile) => {
-										const totalLength = imageFile.length;
-
-										if (totalLength === 0) {
-											return "At least one image should be added";
-										}
-										if (totalLength > 1) {
-											return "Select only one image!";
-										}
-
-										return true;
-									},
-								})}
-							/>
-						</div>
-						{errors.imageFile && (
-							<span className="text-red-500 text-sm font-bold">
-								{errors.imageFile.message}
-							</span>
-						)}
+				<div className="flex flex-col gap-2">
+					<h1 className="text-4xl font-bold font-grotesk text-lightgreen max-sm:text-xl">
+						Add an Event
+					</h1>
+					<p className="font-poppins text-lg max-sm:text-base">List impactful events for rural communities. Join us in making a difference! Seamless registration, mobile notifications.</p>
+					{/* Seprator */}
+					<div className="h-px my-1 bg-black w-full">
 					</div>
-				</label>
-				<span className="flex mx-auto">
-					<button
-						type="submit"
-						className="bg-green-600 text-white py-2 font-bold hover:bg-green-500 text-xl px-6 border rounded-lg"
-					>
-						Add Event
-					</button>
-				</span>
+				</div>
+				<div className="p-4 border rounded-lg font-grotesk flex max-sm:flex-col justify-between items-center">
+					<div>
+						<img src={addEvent} alt="add event illustration" className="pointer-events-none" width={500} height={500} />
+					</div>
+					<div className="w-1/2 max-sm:w-full p-4 border rounded-md flex gap-2 flex-col items-start">
+						<div>
+							<h1 className="text-xl font-bold font-grotesk text-lightyellow max-sm:text-lg">
+								Event Description
+							</h1>
+							<p className="font-poppins text-base max-sm:text-sm ">Provide a brief overview of the event, including its purpose, activities, and how it will benefit rural communities. Be concise yet informative.</p>
+							{/* Seprator */}
+							<div className="h-px my-1 bg-black w-full">
+							</div>
+							<label className="text-gray-700 text-md font-bold flex-1">
+								Name
+								<input
+									type="text"
+									className="border rounded w-full py-1 px-2 font-normal"
+									{...register("name", {
+										required: "Field is required",
+									})}
+								/>
+								{errors.name && (
+									<span className="text-red-500 text-sm">
+										{errors.name.message}
+									</span>
+								)}
+							</label>
+
+							<div className="flex gap-4">
+								<label className="text-gray-700 text-md font-bold flex-1">
+									Organizer Name
+									<input
+										type="text"
+										className="border rounded w-full py-1 px-2 font-normal"
+										{...register("organizer", {
+											required: "Field is required",
+										})}
+									/>
+									{errors.organizer && (
+										<span className="text-red-500 text-sm">
+											{errors.organizer.message}
+										</span>
+									)}
+								</label>
+								<label className="text-gray-700 text-md font-bold flex-1">
+									Venue
+									<input
+										type="text"
+										className="border rounded w-full py-1 px-2 font-normal"
+										{...register("venue", {
+											required: "Field is required",
+										})}
+									/>
+									{errors.venue && (
+										<span className="text-red-500 text-sm">
+											{errors.venue.message}
+										</span>
+									)}
+								</label>
+							</div>
+
+							<label className="text-gray-700 text-md font-bold flex-1">
+								Description
+								<textarea
+									rows={10}
+									className="border rounded w-full py-1 px-2 font-normal"
+									{...register("description", {
+										required: "Field is required",
+									})}
+								/>
+								{errors.description && (
+									<span className="text-red-500 text-sm">
+										{errors.description.message}
+									</span>
+								)}
+							</label>
+							<div className="flex gap-4 max-sm:flex-col">
+								<label className="text-gray-700 text-md font-bold flex-1">
+									Select Time
+									<input
+										type="time"
+										className="border rounded w-full py-1 px-2 font-normal"
+										{...register("time", {
+											required: "Field is required",
+										})}
+									/>
+									{errors.time && (
+										<span className="text-red-500 text-sm">
+											{errors.time.message}
+										</span>
+									)}
+								</label>
+								<label className=" text-md font-bold">
+									Event Date
+									<DatePicker
+										required
+										selected={eventDate}
+										onChange={(date) => setValue("eventDate", date)}
+										selectsStart
+										startDate={eventDate}
+										minDate={minDate}
+										maxDate={maxDate}
+										placeholderText="Enter Date"
+										className="bg-white p-1 focus:outline:none border rounded-md "
+										wrapperClassName="datePicker"
+									/>
+								</label>
+							</div>
+							{/* Images logic */}
+							<label className="text-gray-700 text-md font-bold ">
+								<div>
+									<h2 className="text-2xl max-sm:text-lg font-bold mb-3">
+										Upload Image for the event
+									</h2>
+									<div className="border rounded p-4 flex flex-col gap-4">
+										<input
+											type="file"
+											multiple
+											accept="image/*"
+											className="w-full text-gray-700 font-normal"
+											{...register("imageFile", {
+												validate: (imageFile) => {
+													const totalLength = imageFile.length;
+
+													if (totalLength === 0) {
+														return "At least one image should be added";
+													}
+													if (totalLength > 1) {
+														return "Select only one image!";
+													}
+
+													return true;
+												},
+											})}
+										/>
+									</div>
+									{errors.imageFile && (
+										<span className="text-red-500 text-sm font-bold">
+											{errors.imageFile.message}
+										</span>
+									)}
+								</div>
+							</label>
+						</div>
+						<span className="flex mx-auto">
+							<button
+								type="submit"
+								className="text-lg bg-lightgreen py-1 px-6 text-black font-semibold font-grotesk
+								rounded-lg  hover:bg-lightyellow  hover:text-white transition-all"
+							>
+								Add Event
+							</button>
+						</span>
+					</div>
+				</div>
 			</form>
 		</FormProvider>
 	);
